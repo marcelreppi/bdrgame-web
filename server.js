4419da023b792d97e57aded4f8bd838c74267d04
Marcel Reppenhagen <marcelreppi@gmail.com>
2018-05-05 15:40:33 +0200

    player moves are broadcasted

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const axios = require('axios')
 const express = require('express')
 const bodyParser = require('body-parser');
 const http = require('http')
-const io = require('socket.io');
 
 const { addTokenProps } = require('./data')
 
 const app = express()
 const server = http.createServer(app);
-const socket = io(server)
+const io = require('socket.io')(server)
 
 const BACKEND_URL = 'https://bdrgame-backend.herokuapp.com'
 
@@ -16,9 +15,9 @@ const BACKEND_URL = 'https://bdrgame-backend.herokuapp.com'
 
 app.use(bodyParser.json());
 
-socket.on('connection', client => {
-  console.log(`player ${client.id} has connected`)
-  client.on('initialData', (callback) => {
+io.on('connection', socket => {
+  console.log(`player ${socket.id} has connected`)
+  socket.on('initialData', (callback) => {
     console.log('sending initial data')
 
     const tokenPromise = axios.get(BACKEND_URL + '/tokens')
@@ -29,18 +28,21 @@ socket.on('connection', client => {
       })
   })
 
-  client.on('movePlayer', (id, x, y, callback) => {
+  socket.on('movePlayer', (id, x, y, callback) => {
     console.log(`player ${id} wants to move to ${x} ${y}`)
     axios.put(BACKEND_URL + `/players/${id}/move`, { x, y })
-      .then( res => console.log(`player ${id} successfully move to ${x} ${y}`) )
+      .then( () => {
+        console.log(`player ${id} successfully move to ${x} ${y}`)
+        socket.broadcast.emit('playerMove', { id, x, y })
+        callback(true)
+      })
       .catch( err => console.error(`player ${id} move to ${x} ${y} failed`) )
     // contact backend and check the move
-    callback(true)
   })
 
   app.post('/players/move', (req, res) => {
     console.log('move player')
-    client.emit('playerMove', req.body )
+    socket.emit('playerMove', req.body )
     res.end('moved player')
   })
 })
